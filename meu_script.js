d5e1eb2363f18641323d03a41cc2d93c722026ca
Matheus Felippe Donato Escobar <mescobar@e-ig-1720s-iMac.local>
2018-07-24 13:36:15 -0300

    Commit final do primeiro módulo do curso

--- meu_script.js
+++ meu_script.js
@@ -1,3 +1,9 @@
-alert('Olá, seja bem vindo ao curso!')
+//alert('Olá, seja bem vindo ao curso!')
+
+/*
+	Selecionar um elemento DOM
+	atualizar o valor desse elemento com uma string
+
+*/
+
 document.getElementById('nome').value = 'Oi'
-		
